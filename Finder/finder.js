a9b9892f361979040a7fb56b478e59c55383d574
Rajat Menon <22711531@student.uwa.edu.au>
2022-05-02 15:51:51 +0800

    Update

--- Finder/finder.js
+++ Finder/finder.js
@@ -1,3 +1,13 @@
 function addFinder(){
-       
+    let text = document.getElementById("para");
+    let textValue = text.value;
+    let search = document.getElementById("search");
+
+    if(textValue.search(search.value) != -1){
+        console.log()
+        // let add = document.getElementById("add");
+        // add.innerHTML = search.value;
+
+    }
+    //console.log();
 }
